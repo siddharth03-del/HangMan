2d84a44060ec67a1431e269d9647c0dc8a45c035
siddharth03-del <148877613+siddharth03-del@users.noreply.github.com>
2024-08-06 19:28:05 +0530

    HangMan uploaded

--- src/Keypad.jsx
+++ src/Keypad.jsx
@@ -5,7 +5,7 @@ function Keypad({clickhandler}){
         <div className="mt-10 w-3/5 ml-10">
             {keys.map((key, index)=>{
                 return(
-                    <button className="h-16 w-16 border-4 border-blue-600 rounded-2xl bg-white mx-3 my-3" id={`${key}-${index}`} key={`${key}-${index}`} value={key} onClick={()=>{
+                    <button className="h-16 w-16 border-4 border-blue-600 rounded-2xl bg-white mx-3 my-3 hover:bg-blue-300" id={`${key}-${index}`} key={`${key}-${index}`} value={key} onClick={()=>{
                         clickhandler(event);
                     }}>{key}</button>
                 )
